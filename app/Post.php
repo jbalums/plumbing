ce2cc776e72ddc68699c5f23617bbc9d3c430bd8
unknown <earlbulasa@gmail.com>
2017-11-06 12:34:27 +0800

    database

---
+++ app/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function photos(){
+      return $this->hasMany('App\Photo');
+    }
+}
